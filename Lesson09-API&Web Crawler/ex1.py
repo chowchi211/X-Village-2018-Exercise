d327bc79bdff58d2b9e6d031975e1990210ca4b0
chowchi211 <fantacat211@gmail.com>
2018-07-26 12:57:18 +0800

    modified ex1.py

--- Lesson09-API&Web Crawler/ex1.py
+++ Lesson09-API&Web Crawler/ex1.py
@@ -14,14 +14,19 @@ with open ('weather.json', 'r') as g:
     # print(type(show))
     
 
-print('Today\'s weather is:')
-print('='*50 )
+print('Today\'s weather is: ', end='')
+print(show['consolidated_weather'][0]['weather_state_name']) 
+print('uploaded time: ', show['consolidated_weather'][0]['applicable_date'])
 
-for i in show['consolidated_weather']:
+# print('='*50 )
+
+# for i in show['consolidated_weather']:
     
     
-    print('WeatherType:', i['weather_state_name']) 
-    print('UploadTime:', i['created'])
-    print('temp:', i['the_temp']) 
+#     print('WeatherType:', i['weather_state_name']) 
+#     print('UploadTime:', i['created'])
+#     print('temp:', i['the_temp']) 
     
-    print('='*50 )
+#     print('='*50 )
+
+
